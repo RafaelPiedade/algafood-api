02a827a7f876705b1a41d89cd7294e105253ff5e
RafaelPiedade <rafaeldade@hotmail.com>
2023-02-07 22:18:39 -0300

    DI: refactory AtivacaoCleinteService to injection @Autowired optional

--- src/main/java/com/algaworks/algafoodapi/NotificacaoConfig.java
+++ src/main/java/com/algaworks/algafoodapi/NotificacaoConfig.java
@@ -4,12 +4,12 @@ import com.algaworks.algafoodapi.di.notificacao.NotificadorEmail;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class NotificacaoConfig {
-    @Bean
-    public NotificadorEmail notificadorEmail(){
-        NotificadorEmail notificador = new NotificadorEmail("smtp.algamail.com.br");
-        notificador.setCaixaAlta(true);
-        return notificador;
-    }
-}
+//@Configuration
+//public class NotificacaoConfig {
+//    @Bean
+//    public NotificadorEmail notificadorEmail(){
+//        NotificadorEmail notificador = new NotificadorEmail("smtp.algamail.com.br");
+//        notificador.setCaixaAlta(true);
+//        return notificador;
+//    }
+//}
--- src/main/java/com/algaworks/algafoodapi/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafoodapi/di/service/AtivacaoClienteService.java
@@ -8,25 +8,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class AtivacaoClienteService {
 
-    @Autowired
+    @Autowired(required = false)
     private Notificador notificador;
-
-//    @Autowired
-//    public AtivacaoClienteService(Notificador notificador) {
-//        this.notificador = notificador;
-//    }
-
-//    public AtivacaoClienteService(String qualquer) {
-//    }
-
 
     public void ativarCliente(Cliente cliente) {
         cliente.ativar();
-        notificador.notificar(cliente, "Seu cadastro no sistema está ativo");
+
+        if(notificador != null){
+            notificador.notificar(cliente, "Seu cadastro no sistema está ativo");
+        }else{
+            System.out.println("Não existe notificador, mas cliente foi ativado");
+        }
+
     }
-//    @Autowired
-//    public void setNotificador(Notificador notificador) {
-//        this.notificador = notificador;
-//    }
 }
 
