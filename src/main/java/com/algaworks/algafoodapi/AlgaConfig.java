2a6231c157457e2db3e818510cefc128d34216e0
RafaelPiedade <rafaeldade@hotmail.com>
2023-02-07 21:32:28 -0300

    DI: refactory NoticadorEmail DI to file AlgaConfig @Configuration and created method with annotation @Bean

---
+++ src/main/java/com/algaworks/algafoodapi/AlgaConfig.java
@@ -0,0 +1,18 @@
+package com.algaworks.algafoodapi;
+
+import com.algaworks.algafoodapi.di.notificacao.NotificadorEmail;
+import com.algaworks.algafoodapi.di.service.AtivacaoClienteService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AlgaConfig {
+
+    @Bean
+    public NotificadorEmail notificadorEmail(){
+        NotificadorEmail notificador = new NotificadorEmail("smtp.algamail.com.br");
+        notificador.setCaixaAlta(true);
+        return notificador;
+    }
+
+}
--- src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmail.java
@@ -1,13 +1,26 @@
 package com.algaworks.algafoodapi.di.notificacao;
 
 import com.algaworks.algafoodapi.di.modelo.Cliente;
-import org.springframework.stereotype.Component;
 
-@Component
 public class NotificadorEmail implements Notificador {
+
+    private boolean caixaAlta;
+    private String hostServidorSmtp;
+
+    public NotificadorEmail( String hostServidorSmtp) {
+        this.hostServidorSmtp = hostServidorSmtp;
+        System.out.println("NotificadorEmail");
+    }
 
     @Override
     public void notificar(Cliente cliente, String message){
-        System.out.printf("Notificando %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), message);
+        if(caixaAlta){
+            message = message.toUpperCase();
+        }
+        System.out.printf("Notificando %s através do e-mail %s usando SMTP %s: %s\n", cliente.getNome(), cliente.getEmail(), this.hostServidorSmtp, message);
+    }
+
+    public void setCaixaAlta(boolean caixaAlta) {
+        this.caixaAlta = caixaAlta;
     }
 }
