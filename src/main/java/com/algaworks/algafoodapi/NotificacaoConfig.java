37102984d9fb3520ce8ac8c226e149d102529406
RafaelPiedade <rafaeldade@hotmail.com>
2023-02-07 21:43:49 -0300

    DI: refactory AlgaConfig, separeted configurate NofiticacaoConfig and ServiceConfig

--- src/main/java/com/algaworks/algafoodapi/NotificacaoConfig.java
+++ src/main/java/com/algaworks/algafoodapi/NotificacaoConfig.java
@@ -1,24 +1,15 @@
 package com.algaworks.algafoodapi;
 
 import com.algaworks.algafoodapi.di.notificacao.NotificadorEmail;
-import com.algaworks.algafoodapi.di.service.AtivacaoClienteService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AlgaConfig {
-
+public class NotificacaoConfig {
     @Bean
     public NotificadorEmail notificadorEmail(){
         NotificadorEmail notificador = new NotificadorEmail("smtp.algamail.com.br");
         notificador.setCaixaAlta(true);
         return notificador;
     }
-
-    @Bean
-    public AtivacaoClienteService ativacaoClienteService(){
-        AtivacaoClienteService ativacaoClienteService = new AtivacaoClienteService(this.notificadorEmail());
-        return ativacaoClienteService;
-    }
-
 }
