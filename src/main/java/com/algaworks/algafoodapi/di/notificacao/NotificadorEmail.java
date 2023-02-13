92318e7a03389e05c1420b96d0f2544ab08c5185
RafaelPiedade <rafaeldade@hotmail.com>
2023-02-13 19:45:46 -0300

    DI: solving ambiguity problem with @Qualifier

--- src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmail.java
@@ -1,10 +1,10 @@
 package com.algaworks.algafoodapi.di.notificacao;
 
 import com.algaworks.algafoodapi.di.modelo.Cliente;
-import org.springframework.context.annotation.Primary;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-@Primary
+@Qualifier("email")
 @Component
 public class NotificadorEmail implements Notificador {
 
--- src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorSMS.java
+++ src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorSMS.java
@@ -1,11 +1,12 @@
 package com.algaworks.algafoodapi.di.notificacao;
 
 import com.algaworks.algafoodapi.di.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class NotificadorSMS implements Notificador {
-
+    @Qualifier("sms")
     @Override
     public void notificar(Cliente cliente, String message){
         System.out.printf("Notificando %s por SMS através do telefone %s: %s\n", cliente.getNome(), cliente.getTelefone(), message);
