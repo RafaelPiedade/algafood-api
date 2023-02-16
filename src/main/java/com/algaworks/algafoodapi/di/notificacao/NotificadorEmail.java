9b79782cf931244a6346d1ed3c653bf8ba83e28f
RafaelPiedade <rafaeldade@hotmail.com>
2023-02-16 20:43:52 -0300

    DI: setting environment variables and accessing them with @Value

--- src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmail.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafoodapi.di.notificacao;
 
 import com.algaworks.algafoodapi.di.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -9,8 +10,17 @@ import org.springframework.stereotype.Component;
 @Component
 public class NotificadorEmail implements Notificador {
 
+    @Value("${notificador.email.host-servidor}")
+    private String host;
+
+    @Value("${notificador.email.port-servidor}")
+    private Integer port;
+
+
     @Override
     public void notificar(Cliente cliente, String message){
+        System.out.println("Host: "+host);
+        System.out.println("Port: "+port);
         System.out.printf("Notificando %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), message);
     }
 
