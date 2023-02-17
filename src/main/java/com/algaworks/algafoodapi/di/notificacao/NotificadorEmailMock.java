fd442e873b12729f39974b8a6ba005969762c717
RafaelPiedade <rafaeldade@hotmail.com>
2023-02-16 21:10:32 -0300

    DI: changing project configuration depending on environment (with Spring Profiles)

--- src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmailMock.java
+++ src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmailMock.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafoodapi.di.notificacao;
 
 import com.algaworks.algafoodapi.di.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -8,10 +9,13 @@ import org.springframework.stereotype.Component;
 @TipoDeNotificador(NivelUrgencia.NORMAL)
 @Component
 public class NotificadorEmailMock implements Notificador {
+    @Autowired
+    private NotificadorProperties properties;
 
     @Override
     public void notificar(Cliente cliente, String message){
-
+        System.out.println("Host: "+properties.getHostServidor());
+        System.out.println("Port: "+properties.getPortaServidor());
         System.out.printf("MOCK: Notificação seria enviada para %s através do e-mail %s: %s\n", cliente.getNome(), cliente.getEmail(), message);
     }
 
